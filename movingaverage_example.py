71ef17233aebe2f502400ee0525f78249b7195a0
nobreconfrade <willpereirabr@gmail.com>
2018-02-20 10:12:29 -0300

    moving average

--- movingaverage_example.py
+++ movingaverage_example.py
@@ -1,5 +1,5 @@
 import datetime as dt
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
@@ -8,4 +8,16 @@ style.use('ggplot')
 # df = data frame
 dataf = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
 
-dataf['100ma']
+dataf['100ma'] = dataf['Close'].rolling(window=100,min_periods=0).mean() #mean() é media
+# dataf.dropna(inplace=True)
+
+print(dataf.tail())
+
+ax1 = plt.subplot2grid((6,1),(0,0),rowspan = 5, colspan=1)
+ax2 = plt.subplot2grid((6,1),(5,0),rowspan = 1, colspan=1, sharex=ax1)
+
+ax1.plot(dataf.index, dataf['Close'])
+ax1.plot(dataf.index, dataf['100ma'])
+ax2.bar(dataf.index, dataf['Volume'])
+
+plt.show()
